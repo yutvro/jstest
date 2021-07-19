50cab269e43c40779d0a7908d62c308eb1525fe2
liu269569205 <liu269569205@gmail.com>
2021-07-19 14:06:26 +0800

    test

--- sendNotify.js
+++ sendNotify.js
@@ -215,6 +215,9 @@ async function sendNotify(text, desp, params = {}) {
 		await sendNotify1(text,des3,params)
 			if(des4)
 		await sendNotify1(text,des4,params)
+			await Promise.all([
+    pushPlusNotify(text, desp)//pushplus(推送加)
+  ])
 }
 async function sendNotify1(text, desp, params = {}) {
   await sleep(1000);
@@ -222,7 +225,7 @@ async function sendNotify1(text, desp, params = {}) {
   desp += ``;
   await Promise.all([
     serverNotify(text, desp),//微信server酱
-    pushPlusNotify(text, desp)//pushplus(推送加)
+  //  pushPlusNotify(text, desp)//pushplus(推送加)
   ])
   //由于上述两种微信通知需点击进去才能查看到详情，故text(标题内容)携带了账号序号以及昵称信息，方便不点击也可知道是哪个京东哪个活动
   text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
