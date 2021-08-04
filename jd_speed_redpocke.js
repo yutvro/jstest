ad6e0eb13732173a2f72a795c698645245c4570f
liujingjing <j6219519>
2021-08-04 17:26:05 +0800

    test

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -77,7 +77,7 @@ async function jsRedPacket() {
   try {
     await invite();
     await sign();//极速版签到提现
-    await reward_query();
+    //await reward_query();
     for (let i = 0; i < 3; ++i) {
       await redPacket();//开红包
       await $.wait(2000)
