f6fed09bd9593cbeaabb86d6b70db8925a527f3c
liu269569205 <liu269569205@gmail.com>
2021-08-27 21:39:12 +0800

    test

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -16,8 +16,8 @@ const exec = require('child_process').execSync
 const fs = require('fs')
 const download = require('download');
 let resultPath = "./result.txt";
-let JD_DailyBonusPath = "./utils/JD_DailyBonus.js";
-let outPutUrl = './utils';
+let JD_DailyBonusPath = "./JD_DailyBonus.js";
+let outPutUrl = './';
 let NodeSet = 'CookieSet.json';
 let cookiesArr = [], cookie = '', allMessage = '', jrBodyArr = [], jrBody = '';
 
