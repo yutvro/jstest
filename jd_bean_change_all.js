46f0cc1f84384c11579fc7073ab73b21c3a76def
liujingjing <j6219519>
2021-10-18 17:01:54 +0800

    test

--- jd_bean_change_all.js
+++ jd_bean_change_all.js
@@ -99,7 +99,7 @@ if ($.isNode()) {
 			  }
 			  if ($.isNode() && allMessage) {
 				await notify.sendNotify(`${$.name}`, `${allMessage}`, { url: `https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean` })
-				return
+				
 			  }  
 			  allReceiveMessage="";
 			  allMessage="";
