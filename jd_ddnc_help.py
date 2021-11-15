d196fc7ee89b27b90d60fa7874965e28f825a4bc
liujingjing <j6219519>
2021-11-15 17:56:40 +0800

    test

--- jd_ddnc_help.py
+++ jd_ddnc_help.py
@@ -11,7 +11,7 @@ cron: 0 0 * * *
 new Env('东东农场-助力');
 '''
 # 是否按ck顺序助力, true: 按顺序助力 false：按指定用户助力，默认true
-ddnc_isOrder="false"
+ddnc_isOrder="true"
 # 东东农场助力名单(当ddnc_isOrder="false" 才生效), ENV 环境设置 export ddnc_help_list="Curtinlv&用户2&用户3"
 ddnc_help_list = ["18014246678_p", "jd_FdDjJBENiJzA", "18915299015_p"]
 #是否开启通知，Ture：发送通知，False：不发送
