3d4be07b494a9fa5a5a14467ef2311509b3c5f46
liujingjing <j6219519>
2021-11-24 17:59:48 +0800

    test

--- zqydCOOKIE.js
+++ zqydCOOKIE.js
@@ -12,7 +12,7 @@ module.exports = {
     }, {
       "id": "zqydCount",
       "name": "账号个数",
-      "val": "3",
+      "val": process.env.zqydkkz_num?process.env.zqydkkz_num:1,
       "type": "number",
       "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
     }, {
