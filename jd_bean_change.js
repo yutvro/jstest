96f1b996cda9dfde4ba57593b25bedd922a2bc91
liujingjing <j6219519>
2021-12-21 13:49:52 +0800

    test

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -52,7 +52,7 @@ let ReturnMessageTitle="";
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let intPerSent = 20;
+let intPerSent = 0;
 let i = 0;
 let DisableCash = "false";
 let llShowMonth = false;
@@ -803,7 +803,9 @@ async function showMsg() {
 	console.log(`${ReturnMessageTitle+ReturnMessage}`);
 
 	if ($.isNode() && WP_APP_TOKEN_ONE) {
+		var strTitle="京东资产变动";
 		if (TempBaipiao) {
+			strTitle="京东资产变动及活动领取";
 			TempBaipiao = `【⏰商品白嫖活动提醒⏰】\n` + TempBaipiao;
 			ReturnMessage = TempBaipiao + `\n` + ReturnMessage;
 		}
@@ -812,7 +814,7 @@ async function showMsg() {
 		if(strAllNotify)
 			ReturnMessage=strAllNotify+`\n`+ReturnMessage;
 		
-		await notify.sendNotifybyWxPucher(`${$.name}`, `${ReturnMessage}`, `${$.UserName}`);
+		await notify.sendNotifybyWxPucher(strTitle, `${ReturnMessage}`, `${$.UserName}`);
 	}
 
 	//$.msg($.name, '', ReturnMessage , {"open-url": "https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean"});
