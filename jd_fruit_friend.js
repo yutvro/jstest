ca20a6ad77932c9f33f4c2a94d7997ed6584ee76
liujingjing <j6219519>
2022-02-21 10:45:02 +0800

    test

--- jd_fruit_friend.js
+++ jd_fruit_friend.js
@@ -10,16 +10,19 @@
 ==========================Quantumultx=========================
 [task_local]
 #jd免费水果
-40 4,19 * * * jd_fruit_friend.js, tag=东东农场好友删减奖励, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
+10 5,17 * * * jd_fruit_friend.js, tag=东东农场好友删减奖励, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
 =========================Loon=============================
 [Script]
-cron "40 4,19 * * *" script-path=jd_fruit_friend.js,tag=东东农场好友删减奖励
+cron "10 5,17 * * *" script-path=jd_fruit_friend.js,tag=东东农场好友删减奖励
 
 =========================Surge============================
 东东农场好友删减奖励 = type=cron,cronexp="10 5,17 * * *",wake-system=1,timeout=3600,script-path=jd_fruit_friend.js
 
 =========================小火箭===========================
 东东农场好友删减奖励 = type=cron,script-path=jd_fruit_friend.js, cronexpr="10 5,17 * * *", timeout=3600, enable=true
+
+每号间隔（毫秒），默认0毫秒（0分钟）
+export fruit_sleep=20000
 
 */
 const $ = new Env('东东农场好友删减奖励');
@@ -88,6 +91,9 @@ let llhelp=true;
       $.retry = 0;
       await jdFruit();
     }
+		if ($.isNode()) {
+		process.env.fruit_sleep ? await $.wait(Number(process.env.fruit_sleep)) : ''
+		}
   }
   if ($.isNode() && allMessage && $.ctrTemp) {
     await notify.sendNotify(`${$.name}`, `${allMessage}`)
@@ -114,8 +120,8 @@ async function jdFruit() {
     console.log(`任务执行异常，请检查执行日志 ‼️‼️`);
     $.logErr(e);
 	const errMsg = `京东账号${$.index} ${$.nickName || $.UserName}\n任务执行异常，请检查执行日志 ‼️‼️`;
-    //if ($.isNode()) await notify.sendNotify(`${$.name}`, errMsg);
-    //$.msg($.name, '', `${errMsg}`)
+    if ($.isNode()) await notify.sendNotify(`${$.name}`, errMsg);
+    $.msg($.name, '', `${errMsg}`)
   }
   //await showMsg();
 }
@@ -170,8 +176,8 @@ async function GetCollect() {
       console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}互助码】${$.farmInfo.farmUserPro.shareCode}`);
       newShareCodes.push($.farmInfo.farmUserPro.shareCode)
     } else {
-      console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}互助码】\n数据异常,使用作者的互助码:84d724474fc944d9a36939838d1908c1`);
-	  newShareCodes.push("84d724474fc944d9a36939838d1908c1");
+      console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}互助码】\n数据异常,使用作者的互助码:94f3ba0769b04c94bbfca26139108702`);
+	  newShareCodes.push("94f3ba0769b04c94bbfca26139108702");
     }
   } catch (e) {
     $.logErr(e);
--- jd_opencardL79.js
+++ jd_opencardL79.js
@@ -1,26 +1,26 @@
 /*
-2.18~2.23 联合开卡 
+2.20~2.26 联合开卡 
 新增开卡脚本，一次性脚本
 
 第一个账号助力作者 其他依次助力CK1
 第一个CK失效会退出脚本
 
 ————————————————
-入口：[ 2.18~2.23 联合开卡]
+入口：[ 2.20~2.26 联合开卡 ]
 
 请求太频繁会被黑ip
 过10分钟再执行
 
 
-cron:35 0,14 18-23 2 *
+cron:41 0,17 20-26 2 *
 ============Quantumultx===============
 [task_local]
-#2.18~2.23 联合开卡
-35 0,14 18-23 2 * jd_opencardL75.js, tag=2.18~2.23 联合开卡, enabled=true
+#2.20~2.26 联合开卡
+41 0,17 20-26 2 * jd_opencardL79.js, tag=2.20~2.26 联合开卡, enabled=true
 
 */
 
-const $ = new Env('2.18~2.23 联合开卡');
+const $ = new Env('2.20~2.26 联合开卡');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 
@@ -53,10 +53,10 @@ let activityCookie =''
   // return
   $.appkey = '51B59BB805903DA4CE513D29EC448375'
   $.userId = '10299171'
-  $.actId = 'dd01d7abe65a4bac83967e94968_220218'
+  $.actId = '29a7e74b884d42adb90d8_22022001'
   $.MixNicks = ''
   $.inviteNick = 'k1Nobb+P0er+C2sysxnx/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ/e9r/uf'
-  console.log(`活动地址:https://jinggengjcq-isv.isvjcloud.com/fronth5/#/pages/unitedCardNew20220218/unitedCardNew20220218?actId=dd01d7abe65a4bac83967e94968_220218`)
+  console.log(`活动地址:https://jinggengjcq-isv.isvjcloud.com/fronth5/#/pages/unitedCardNew20220220/unitedCardNew20220220?actId=29a7e74b884d42adb90d8_22022001`)
   console.log(`请自行测试有水无水。`)
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
